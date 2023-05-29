8610c507d5c70bdaa99b6331b04e78f02fd3245a
leonardoGomes <leonardojpinheirogomes@gmail.com>
2023-05-28 22:39:09 -0300

    feat: ChessPosition

--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -1,5 +1,7 @@
 package chess;
 
+import boardgame.Position;
+
 public class ChessPosition {
 	private char column;
 	private int row;
@@ -11,7 +13,29 @@ public class ChessPosition {
 		this.column = column;
 		this.row = row;
 	}
+
+	public char getColumn() {
+		return column;
+	}
+
+
+	public int getRow() {
+		return row;
+	}
+
 	
+	protected Position toPosition() {
+		return new Position(8 - row, column - 'a');
+	}
+	
+	protected static ChessPosition fromPosition(Position position) {
+		return new ChessPosition((char) ('a' - position.getColumn()), 8 - position.getRow());
+	}
+	
+	@Override
+	public String toString() {
+		return "" + column + row;
+	}
 	
 	
 }
