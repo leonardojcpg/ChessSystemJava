9a7fc3f34b36b074a93b49f29a6b8b19d4d962e6
leonardoGomes <leonardojpinheirogomes@gmail.com>
2023-06-01 20:26:42 -0300

    feat: Performing chessMove

--- src/application/UI.java
+++ src/application/UI.java
@@ -28,7 +28,7 @@ public class UI {
 	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 	
-	// reading chess position
+	// reading chess position 
 	public static ChessPosition readChessPosition(Scanner sc) {
 		try {
 			String s = sc.nextLine();
